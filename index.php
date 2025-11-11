41f0fbb2ba0c23fc9c8603fd4418ed7308ffc864
noorjaveria976 <noorjaveria976@gmail.com>
2025-11-11 22:52:29 +0500

    Updated

--- index.php
+++ index.php
@@ -77,6 +77,7 @@ include('config.php');
             <option>Lahore</option>
             <option>Karachi</option>
             <option>Islamabad</option>
+            <option>Islamabad</option>
           </select>
           <button class="btn btn-primary mb-2">Search</button>
         </form>
