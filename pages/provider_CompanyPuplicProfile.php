6bfbf8b6f12dda5f71a392eb1f77f5c54d67b164
noorjaveria976 <noorjaveria976@gmail.com>
2025-11-14 00:04:10 +0500

    updated

--- pages/provider_CompanyPuplicProfile.php
+++ pages/provider_CompanyPuplicProfile.php
@@ -28,7 +28,7 @@ include('config.php');
                     }
                 </script>
                 <!-- Job Header start -->
-                <div class="row">
+                <div class="row main">
                     <div class="col-lg-7">
 
 
